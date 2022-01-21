cc8fcb4d968ec2cf344ed3051919a750d3d5450a
kskashish023 <90233544+kskashish023@users.noreply.github.com>
2022-01-21 11:39:54 +0530

    Add files via upload

---
+++ onlinefood-order/partials-front/footer.php
@@ -0,0 +1,28 @@
+<!-- social Section Starts Here -->
+<section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="https://www.facebook.com/profile.php?id=100010860335998"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://instagram.com/__kashish__x__?utm_medium=copy_link"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/Kashish55708214?t=vGV8DktGnoU3nv8x6EgV7Q&s=09"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>All rights reserved. Designed By KASHISH SHARMA AND TEAM</p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
